06df8db9a7494466e4f542ace9a3ef09ef2cc94c
Yasho <114895377+Yashohara128@users.noreply.github.com>
2026-08-28 11:25:43 +0530

    Change model version from llama-3.3 to llama-3.1

--- api/shortnotes.js
+++ api/shortnotes.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama-3.3-70b-versatile",
+                model: "llama-3.1-70b-versatile", // 👈 මෙන්න මෙතන මොඩල් නම වෙනස් කළා
                 messages: [
                     {
                         role: "system",
