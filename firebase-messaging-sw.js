8509b574675bf983d6f16cc2b95c27dfc8c2663f
Yasho <114895377+Yashohara128@users.noreply.github.com>
2026-09-02 14:42:55 +0530

    Add Firebase messaging service worker implementation

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBjzE30NZXDZsT-DuC9cBrksOjg0UsQM34",
+  authDomain: "gpa-tracker-29cd2.firebaseapp.com",
+  projectId: "gpa-tracker-29cd2",
+  storageBucket: "gpa-tracker-29cd2.firebasestorage.app",
+  messagingSenderId: "856930093441",
+  appId: "1:856930093441:web:60a026cccbc6ceae120080",
+  measurementId: "G-YELCCNX12Q"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
