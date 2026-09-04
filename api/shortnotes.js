cb76a4ce5940f38c40cc8def7dd149cb441d4765
Yashohara Ganewatta <114895377+Yashohara128@users.noreply.github.com>
2026-09-04 21:54:29 +0530

    Fix syntax error in modelsToTry array

--- api/shortnotes.js
+++ api/shortnotes.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         }
 
         const modelsToTry = [
-            'gemini-3.8-flash'
+            'gemini-3.7-flash'
         ];
 
         // කීස් සහ මොඩල්ස් සියල්ල එක්කෝට් කරමින් පූල් එකක් සකස් කිරීම
